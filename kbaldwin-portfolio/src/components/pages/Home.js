7144cb1e156fbc180d6673e813b41f43c1bbe2fd
KBaldwin2 <kimberley.baldwin@ucalgary.ca>
2023-02-06 17:02:33 -0700

    testing project modal image changes

--- kbaldwin-portfolio/src/components/pages/Home.js
+++ kbaldwin-portfolio/src/components/pages/Home.js
@@ -1,16 +1,15 @@
 import classes from "./Home.module.css";
-import * as React from "react";
+import React from "react";
 
 export default function Home() {
   return (
     <div className={classes.container}>
       <div className={classes.typewriter} style={{ marginRight: "0" }}>
-        <h1>Hi! I'm Kim.</h1>
+        <h1>Hi, I'm Kim!</h1>
       </div>
       <div className={classes.typewriter}>
         <h2>
-        I am a software developer with a passion for programming, design and
-          pursuing big ideas. 
+        I am a software developer with a passion for design and building simple, user-friendly applications.
         </h2>
       </div>
     </div>
